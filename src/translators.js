ea07078ff30ef9ddf2fd187c4e8147992c52636e
Alexander Wallin <office@alexanderwallin.com>
2017-03-13 19:25:30 +0000

    Fixes bad parsing of key/value commas in interpolated variables

--- src/translators.js
+++ src/translators.js
@@ -45,7 +45,7 @@ export const interpolateComponents = (str, scope = {}) => {
         }
 
         let keyName = part.replace(/^\{\{\s/, '').replace(/\s\}\}$/, '')
-        let [scopeKey, scopeChildren] = keyName.split(':', 1)
+        let [scopeKey, scopeChildren] = keyName.split(/:(.+)/)
 
         // No matching scope replacement, return raw string
         if (!scope[scopeKey]) {
