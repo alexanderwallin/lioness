f3ed5dbce84e3f333209e97cc15c21a59ac6d395
Alexander Wallin <office@alexanderwallin.com>
2017-03-13 16:13:46 +0000

    Makes <T> support plural and context

--- src/components/T.js
+++ src/components/T.js
@@ -4,7 +4,7 @@ import { withProps } from 'recompose';
 import { withTranslators } from '../composers.js';
 import * as contextTypes from '../contextTypes.js';
 
-const T = ({ message, children, t, ...scope }) =>
-  <span {...scope}>{t(message || children.toString(), scope)}</span>;
+const T = ({ message, messagePlural, context, count, children, tpn, ...scope }) =>
+  tpn(context, message || children.toString(), messagePlural, count, { ...scope, count });
 
 export default withTranslators(T);
