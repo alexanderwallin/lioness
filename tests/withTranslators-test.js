b4a8b4b18f430544d38916725bda4949af373501
Alexander Wallin <office@alexanderwallin.com>
2017-03-16 18:43:06 +0000

    Adds tests for withTranslators()

---
+++ tests/withTranslators-test.js
@@ -0,0 +1,25 @@
+/* eslint-env mocha */
+
+import React from 'react'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import { mount } from 'enzyme'
+
+import withTranslators from '../src/withTranslators.js'
+import LionessProvider from '../src/components/LionessProvider.js'
+
+chai.use(chaiEnzyme())
+
+describe('withTranslators()', () => {
+  it('passes locale and translators from the context to a Component as props', () => {
+    const MyComponent = withTranslators((...props) => <span {...props} />)
+    const app = mount(
+      <LionessProvider messages={{}} locale="en">
+        <div>
+          <MyComponent />
+        </div>
+      </LionessProvider>
+    )
+    expect(app.find('span').props()['0']).to.contain.all.keys(['locale', 't', 'tn', 'tp', 'tpn', 'tc', 'tcn', 'tcp', 'tcpn'])
+  })
+})
