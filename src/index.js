7cc7e38d8be817f9062779958e3d156e7530be23
Alexander Wallin <office@alexanderwallin.com>
2017-08-08 10:54:36 +0100

    Exports interpolateComponents()

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import LionessProvider from './components/LionessProvider.js'
 import T from './components/T.js'
 import withTranslators from './withTranslators.js'
 
+import { interpolateComponents } from './interpolateComponents.js'
+
 export {
   LionessProvider,
   T,
   withTranslators,
+  interpolateComponents,
 }
