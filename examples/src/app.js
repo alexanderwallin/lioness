88135607440d9fb47ccbefef14c2e63b9f874d54
Alexander Wallin <office@alexanderwallin.com>
2021-03-17 00:35:02 +0100

    Bring examples/ back to life

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,88 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { LionessProvider, T, withTranslators } from '../../dist/index.js';
+import { LionessProvider, T, withTranslators } from '../../dist/index.js'
 
 const messages = {
-  "en": {
-    "charset": "utf-8",
-    "headers": {
-      "project-id-version": "lab",
-      "pot-creation-date": "Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)",
-      "po-revision-date": "2016-04-22 10:01+0000",
-      "last-translator": "alexanderwallin <office@alexanderwallin.com>",
-      "language-team": "English (http://www.transifex.com/alexanderwallin/lab/language/en/)",
-      "content-type": "text/plain; charset=UTF-8",
-      "content-transfer-encoding": "8bit",
-      "language": "en",
-      "plural-forms": "nplurals=2; plural=(n != 1);"
+  en: {
+    charset: 'utf-8',
+    headers: {
+      'project-id-version': 'lab',
+      'pot-creation-date': 'Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)',
+      'po-revision-date': '2016-04-22 10:01+0000',
+      'last-translator': 'alexanderwallin <office@alexanderwallin.com>',
+      'language-team':
+        'English (http://www.transifex.com/alexanderwallin/lab/language/en/)',
+      'content-type': 'text/plain; charset=UTF-8',
+      'content-transfer-encoding': '8bit',
+      language: 'en',
+      'plural-forms': 'nplurals=2; plural=(n != 1);',
     },
-    "translations": {
-      "": {
-        "": {
-          "msgid": "",
-          "comments": {
-            "translator": "\nTranslators:"
+    translations: {
+      '': {
+        '': {
+          msgid: '',
+          comments: {
+            translator: '\nTranslators:',
           },
-          "msgstr": [
-            "Project-Id-Version: lab\nPOT-Creation-Date: Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)\nPO-Revision-Date: 2016-04-22 10:01+0000\nLast-Translator: alexanderwallin <office@alexanderwallin.com>\nLanguage-Team: English (http://www.transifex.com/aleaxnderwallin/lab/language/en/)\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nLanguage: en\nPlural-Forms: nplurals=2; plural=(n != 1);\n"
-          ]
+          msgstr: [
+            'Project-Id-Version: lab\nPOT-Creation-Date: Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)\nPO-Revision-Date: 2016-04-22 10:01+0000\nLast-Translator: alexanderwallin <office@alexanderwallin.com>\nLanguage-Team: English (http://www.transifex.com/aleaxnderwallin/lab/language/en/)\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nLanguage: en\nPlural-Forms: nplurals=2; plural=(n != 1);\n',
+          ],
         },
-        "Close": {
-          "msgid": "Close",
-          "comments": {
-            "reference": "./src/filtering/components/FilterBox.jsx:29"
+        Close: {
+          msgid: 'Close',
+          comments: {
+            reference: './src/filtering/components/FilterBox.jsx:29',
           },
-          "msgstr": [
-            "Close"
-          ]
+          msgstr: ['Close'],
         },
-      }
-    }
+      },
+    },
   },
-  "sv": {
-    "charset": "utf-8",
-    "headers": {
-      "project-id-version": "lab",
-      "pot-creation-date": "Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)",
-      "po-revision-date": "2016-04-22 10:02+0000",
-      "last-translator": "Alexander Wallin <office@alexanderwallin.com>",
-      "language-team": "Swedish (http://www.transifex.com/alexanderwallin/lab/language/sv/)",
-      "content-type": "text/plain; charset=UTF-8",
-      "content-transfer-encoding": "8bit",
-      "language": "sv",
-      "plural-forms": "nplurals=2; plural=(n != 1);"
+  sv: {
+    charset: 'utf-8',
+    headers: {
+      'project-id-version': 'lab',
+      'pot-creation-date': 'Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)',
+      'po-revision-date': '2016-04-22 10:02+0000',
+      'last-translator': 'Alexander Wallin <office@alexanderwallin.com>',
+      'language-team':
+        'Swedish (http://www.transifex.com/alexanderwallin/lab/language/sv/)',
+      'content-type': 'text/plain; charset=UTF-8',
+      'content-transfer-encoding': '8bit',
+      language: 'sv',
+      'plural-forms': 'nplurals=2; plural=(n != 1);',
     },
-    "translations": {
-      "": {
-        "": {
-          "msgid": "",
-          "comments": {
-            "translator": "\nTranslators:\nAlexander Wallin <office@alexanderwallin.com>, 2016"
+    translations: {
+      '': {
+        '': {
+          msgid: '',
+          comments: {
+            translator:
+              '\nTranslators:\nAlexander Wallin <office@alexanderwallin.com>, 2016',
           },
-          "msgstr": [
-            "Project-Id-Version: club-admin\nPOT-Creation-Date: Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)\nPO-Revision-Date: 2016-04-22 10:02+0000\nLast-Translator: Alexander Wallin <office@alexanderwallin.com>\nLanguage-Team: Swedish (http://www.transifex.com/alexanderwallin/lab/language/sv/)\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nLanguage: sv\nPlural-Forms: nplurals=2; plural=(n != 1);\n"
-          ]
+          msgstr: [
+            'Project-Id-Version: club-admin\nPOT-Creation-Date: Fri Apr 22 2016 12:01:59 GMT+0200 (CEST)\nPO-Revision-Date: 2016-04-22 10:02+0000\nLast-Translator: Alexander Wallin <office@alexanderwallin.com>\nLanguage-Team: Swedish (http://www.transifex.com/alexanderwallin/lab/language/sv/)\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nLanguage: sv\nPlural-Forms: nplurals=2; plural=(n != 1);\n',
+          ],
         },
-        "Close": {
-          "msgid": "Close",
-          "comments": {
-            "reference": "./src/filtering/components/FilterBox.jsx:29"
+        Close: {
+          msgid: 'Close',
+          comments: {
+            reference: './src/filtering/components/FilterBox.jsx:29',
           },
-          "msgstr": [
-            "Stäng"
-          ]
+          msgstr: ['Stäng'],
         },
-      }
-    }
-  }
-};
+      },
+    },
+  },
+}
 
 //
-let MyThing = ({ t, message }) =>
-  <p>{t(message)}</p>;
+let MyThing = ({ t, message }) => <p>{t(message)}</p>
 
-MyThing = withTranslators(MyThing);
+MyThing = withTranslators(MyThing)
 
 // Singular/plural
 const Diff = withTranslators(({ tcn, num }) => (
@@ -93,11 +91,14 @@ const Diff = withTranslators(({ tcn, num }) => (
       num: num,
     })}
   </div>
-));
+))
 
 // A clickable number
-const ClickableNumber = () =>
-  <em onClick={() => alert('click')} style={{ cursor: 'pointer' }}>131</em>;
+const ClickableNumber = () => (
+  <em onClick={() => alert('click')} style={{ cursor: 'pointer' }}>
+    131
+  </em>
+)
 
 // A pure interpolated string
 const HoverableItemWithNumber = withTranslators(({ t, itemNumber }) => (
@@ -109,15 +110,19 @@ const HoverableItemWithNumber = withTranslators(({ t, itemNumber }) => (
 /**
  * Example app
  */
-const App = () =>
+const App = () => (
   <LionessProvider locale="sv" messages={messages}>
     <div className="App">
-      <pre>{'<T message="Clap {{ num }} times" num={<ClickableNumber />} />'}</pre>
+      <pre>
+        {'<T message="Clap {{ num }} times" num={<ClickableNumber />} />'}
+      </pre>
       <T message="Clap {{ num }} times" num={<ClickableNumber />} />
 
       <hr />
       <pre>{'<p><T>Hello</T></p>'}</pre>
-      <p><T>Hello</T></p>
+      <p>
+        <T>Hello</T>
+      </p>
 
       <hr />
       <pre>{'<MyThing message="Close" />'}</pre>
@@ -153,6 +158,7 @@ const App = () =>
       <HoverableItemWithNumber itemNumber={1} />
       <HoverableItemWithNumber itemNumber={2} />
     </div>
-  </LionessProvider>;
+  </LionessProvider>
+)
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'))
--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,30 +1,48 @@
-var path = require('path');
+const path = require('path')
+const babelConfig = require('../babel.config.js')
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'app.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/assets/',
-    filename: 'app.js'
+    filename: 'app.js',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cwd: path.resolve(__dirname, '..'),
+            ...babelConfig,
+          },
+        },
       },
       {
         test: /\.json$/,
         include: path.resolve(path.join(__dirname, '..')),
-        loader: 'json'
-      }
-    ]
+        loader: 'json-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json'],
+    modules: [
+      path.resolve(__dirname, '../node_modules'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
   },
-  resolveLoader: {
-    root: path.resolve(__dirname, 'node_modules')
-  }
-};
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    host: '0.0.0.0',
+    port: process.env.PORT || 8111,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+    inline: true,
+  },
+}
