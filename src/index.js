4ba17c8d5a6f1abec7fb2e4f12e919fbe9da5e43
Alexander Wallin <office@alexanderwallin.com>
2021-03-17 02:31:35 +0100

    Export GettextAdapter and useTranslation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+export { default as GettextAdapter } from './adapters/GettextAdapter.js'
 export { default as LionessProvider } from './components/LionessProvider.js'
 export { default as T } from './components/T.js'
 export { default as interpolate } from './interpolate.js'
+export { default as useTranslation } from './useTranslation.js'
 export { default as withTranslation } from './withTranslation.js'
