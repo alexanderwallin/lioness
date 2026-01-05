e8e9148bfe9a5c0b45597c36d3ec7e099604346c
Alexander Wallin <hi@alexanderwallin.com>
2026-01-06 00:33:46 +0100

    Rename TComp.tsx back to T.tsx"

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as createNodeGettextAdapter } from './adapters/createNodeGettextAdapter.js'
 export { default as LionessProvider } from './components/LionessProvider.js'
-export { default as T } from './components/TComp.js'
+export { default as T } from './components/T.js'
 export { default as interpolate } from './interpolate.js'
 export { default as useTranslation } from './useTranslation.js'
 export { default as withTranslation } from './withTranslation.js'
